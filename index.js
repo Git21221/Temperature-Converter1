bb1a879f4d668c9ef8f89304144ec0d6e2af3791
Saikat Das <101005577+Git21221@users.noreply.github.com>
2023-03-07 00:55:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,22 +5,106 @@ const kel = document.getElementById("deg-kel");
 const k = 273.15;
 const r = 459.67;
 cel.oninput = function () {
-    fah.value = ((9 * cel.value) + 160) / 5;
-    kel.value = parseInt(cel.value) + k;
-    ran.value = (((9 * cel.value) + 160) / 5) + 459.67;
+
+    if (cel.value === '') {
+        fah.value = '';
+        kel.value = '';
+        ran.value = '';
+    }
+    else {
+        alert("Please enter a valid number!");
+        kel.value = '';
+        cel.value = '';
+        fah.value = '';
+        ran.value = '';
+    }
+
+    if (cel.value >= 0 && cel.value <= 9)
+        fah.value = ((9 * cel.value) + 160) / 5;
+    
+
+    if (cel.value >= 0 && cel.value <= 9)
+        kel.value = cel.value + k;
+    
+
+
+    if (cel.value >= 0 && cel.value <= 9)
+        ran.value = (((9 * cel.value) + 160) / 5) + 459.67;
+
 }
 fah.oninput = function () {
-    cel.value = ((5 * fah.value) - 160) / 9;
-    ran.value = r + parseInt(fah.value);
-    kel.value = (((5 * fah.value) - 160) / 9) + 273.15;
+    if (fah.value === '') {
+        cel.value = '';
+        kel.value = '';
+        ran.value = '';
+    }
+    else {
+        alert("Please enter a valid number!");
+        kel.value = '';
+        cel.value = '';
+        fah.value = '';
+        ran.value = '';
+    }
+
+    if (fah.value >= 0 && fah.value <= 9)
+        cel.value = ((5 * fah.value) - 160) / 9;
+    
+
+    if (fah.value >= 0 && fah.value <= 9)
+        ran.value = r + fah.value;
+    
+
+    if(fah.value >= 0 && fah.value <= 9)
+        kel.value = (((5 * fah.value) - 160) / 9) + 273.15;
+    
+
 }
 ran.oninput = function () {
-    fah.value = ran.value - 459.67;
-    cel.value = ((5 * (ran.value - 459.65)) - 160) / 9;
-    kel.value = (((5 * (ran.value - 459.65)) - 160) / 9) + 273.15;
+    if (ran.value === '') {
+        cel.value = '';
+        kel.value = '';
+        fah.value = '';
+    }
+    else {
+        alert("Please enter a valid number!");
+        kel.value = '';
+        cel.value = '';
+        fah.value = '';
+        ran.value = '';
+    }
+
+    if (ran.value >= 0 && ran.value <= 9)
+        fah.value = ran.value - 459.67;
+    
+    if(ran.value >= 0 && ran.value <= 9)
+        cel.value = ((5 * (ran.value - 459.65)) - 160) / 9;
+
+    if(ran.value >= 0 && ran.value <= 9)
+        kel.value = (((5 * (ran.value - 459.65)) - 160) / 9) + 273.15;
+    
 }
 kel.oninput = function () {
-    fah.value = ((kel.value - 273.15) * 9/5) + 32;
-    cel.value = kel.value - 273.15;
-    ran.value = ((kel.value - 273.15) * 9/5) + 32 + 459.65;
+    if (kel.value === '') {
+        cel.value = '';
+        fah.value = '';
+        ran.value = '';
+    }
+    else {
+        alert("Please enter a valid number!");
+        kel.value = '';
+        cel.value = '';
+        fah.value = '';
+        ran.value = '';
+    }
+
+    if(kel.value >= 0 && kel.value <= 9)
+        fah.value = ((kel.value - 273.15) * 9 / 5) + 32;
+    
+    if(kel.value >= 0 && kel.value <= 9)
+        cel.value = kel.value - 273.15;
+    
+
+    if(kel.value >= 0 && kel.value <= 9)
+        ran.value = ((kel.value - 273.15) * 9 / 5) + 32 + 459.65;
+    
 }
